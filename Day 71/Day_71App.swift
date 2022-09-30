9a7bc222a49cb68594622ef0b2760820ba574138
GunchaPy <guntars.reiss@gmail.com>
2022-09-30 22:37:48 +0300

    Initial Commit

---
+++ Day 71/Day_71App.swift
@@ -0,0 +1,17 @@
+//
+//  Day_71App.swift
+//  Day 71
+//
+//  Created by Guntars Reiss on 30/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Day_71App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
