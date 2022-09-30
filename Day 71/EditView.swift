afbf09a6ca9fba04cd338cdfc4bc1c9e98cf0fec
GunchaPy <guntars.reiss@gmail.com>
2022-09-30 22:42:43 +0300

    Initial push

---
+++ Day 71/EditView.swift
@@ -0,0 +1,49 @@
+//
+//  EditView.swift
+//  Day 71
+//
+//  Created by Guntars Reiss on 30/09/2022.
+//
+
+import SwiftUI
+
+struct EditView: View {
+    @Environment(\.dismiss) var dismiss
+    var location: Location
+    var onSave: (Location) -> Void
+    
+    @State private var name: String
+    @State private var description: String
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Place name", text: $name)
+                    TextField("Description", text: $description)
+                }
+            }
+            .navigationTitle("Place details")
+            .toolbar {
+                Button("Save") {
+                    var newLocation = location
+                    newLocation.id = UUID()
+                    newLocation.name = name
+                    newLocation.description = description
+                    
+                    onSave(newLocation)
+                    dismiss()
+                }
+            }
+        }
+    }
+    
+    init(location: Location, onSave: @escaping (Location) -> Void) {
+        self.location = location
+        self.onSave = onSave
+        
+        _name = State(initialValue: location.name)
+        _description = State(initialValue: location.description)
+    }
+}
+
